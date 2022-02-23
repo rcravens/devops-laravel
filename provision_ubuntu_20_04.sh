3ea729907aefa5d768863516f8a80f5f3d046404
Cravens, Robert L <Robert.Cravens@21co.com>
2022-02-23 11:24:27 -0600

    provision

--- provision_ubuntu_20_04.sh
+++ provision_ubuntu_20_04.sh
@@ -71,11 +71,11 @@ sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/5.
 sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/5.6/fpm/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/5.6/fpm/php.ini
 
-sudo printf "[openssl]\n" | tee -a /etc/php/5.6/fpm/php.ini
-sudo printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/5.6/fpm/php.ini
+sudo printf "[openssl]\n" | sudo tee -a /etc/php/5.6/fpm/php.ini
+sudo printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | sudo tee -a /etc/php/5.6/fpm/php.ini
 
-sudo printf "[curl]\n" | tee -a /etc/php/5.6/fpm/php.ini
-sudo printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/5.6/fpm/php.ini
+sudo printf "[curl]\n" | sudo tee -a /etc/php/5.6/fpm/php.ini
+sudo printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | sudo tee -a /etc/php/5.6/fpm/php.ini
 
 # Configure FPM
 sudo sed -i "s/user = www-data/user = $username/" /etc/php/5.6/fpm/pool.d/www.conf
@@ -84,3 +84,44 @@ sudo sed -i "s/listen\.owner.*/listen.owner = $username/" /etc/php/5.6/fpm/pool.
 sudo sed -i "s/listen\.group.*/listen.group = $username/" /etc/php/5.6/fpm/pool.d/www.conf
 sudo sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/5.6/fpm/pool.d/www.conf
 
+# PHP 7.0
+sudo apt-get install -y --allow-change-held-packages \
+php7.0-bcmath php7.0-bz2 php7.0-cgi php7.0-cli php7.0-common php7.0-curl php7.0-dba php7.0-dev php7.0-enchant \
+php7.0-fpm php7.0-gd php7.0-gmp php7.0-imap php7.0-interbase php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring \
+php7.0-mcrypt php7.0-mysql php7.0-odbc php7.0-opcache php7.0-pgsql php7.0-phpdbg php7.0-pspell php7.0-readline \
+php7.0-recode php7.0-snmp php7.0-soap php7.0-sqlite3 php7.0-sybase php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-xsl \
+php7.0-zip php7.0-imagick php7.0-memcached php7.0-redis
+
+# Configure php.ini for CLI
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/cli/php.ini
+
+# Configure Xdebug
+sudo echo "xdebug.remote_enable = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+sudo echo "xdebug.remote_connect_back = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+sudo echo "xdebug.remote_port = 9000" >> /etc/php/7.0/mods-available/xdebug.ini
+sudo echo "xdebug.max_nesting_level = 512" >> /etc/php/7.0/mods-available/xdebug.ini
+sudo echo "opcache.revalidate_freq = 0" >> /etc/php/7.0/mods-available/opcache.ini
+
+# Configure php.ini for FPM
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
+
+sudo printf "[openssl]\n" | sudo tee -a /etc/php/7.0/fpm/php.ini
+sudo printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | sudo tee -a /etc/php/7.0/fpm/php.ini
+sudo printf "[curl]\n" | sudo tee -a /etc/php/7.0/fpm/php.ini
+sudo printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | sudo tee -a /etc/php/7.0/fpm/php.ini
+
+# Configure FPM
+sudo sed -i "s/user = www-data/user = vagrant/" /etc/php/7.0/fpm/pool.d/www.conf
+sudo sed -i "s/group = www-data/group = vagrant/" /etc/php/7.0/fpm/pool.d/www.conf
+sudo sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.0/fpm/pool.d/www.conf
+sudo sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.0/fpm/pool.d/www.conf
+sudo sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.0/fpm/pool.d/www.conf
