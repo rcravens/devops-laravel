b04a6ceb84127eb5b954381b56267642d77fffe6
Cravens, Robert L <Robert.Cravens@21co.com>
2022-02-23 17:57:06 -0600

    fix

--- provision/installers/mysql.sh
+++ provision/installers/mysql.sh
@@ -28,14 +28,14 @@ export MYSQL_PWD=$db_root_password
 echo "db_root_password=$db_root_password"
 echo "MYSQL_PWD=$MYSQL_PWD"
 
-sudo bash -c 'mysql --user="root" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_password';"'
-sudo bash -c ' mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"'
-sudo bash -c ' mysql --user="root" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"'
-sudo bash -c ' mysql --user="root" -e "CREATE USER 'homestead'@'%' IDENTIFIED BY 'secret';"'
-sudo bash -c ' mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'0.0.0.0' WITH GRANT OPTION;"'
-sudo bash -c ' mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'%' WITH GRANT OPTION;"''
-sudo bash -c ' mysql --user="root" -e "FLUSH PRIVILEGES;"''
-# sudo mysql --user="root" -e "CREATE DATABASE homestead character set UTF8mb4 collate utf8mb4_bin;"
+mysql --user="root" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_password';"
+mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+mysql --user="root" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
+mysql --user="root" -e "CREATE USER 'homestead'@'%' IDENTIFIED BY 'secret';"
+mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'0.0.0.0' WITH GRANT OPTION;"
+mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'%' WITH GRANT OPTION;"
+mysql --user="root" -e "FLUSH PRIVILEGES;"
+# mysql --user="root" -e "CREATE DATABASE homestead character set UTF8mb4 collate utf8mb4_bin;"
 
 sudo tee /home/vagrant/.my.cnf <<EOL
 [mysqld]
