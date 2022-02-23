cb32f19330c1d8a2f4349d182ce761be870de848
Cravens, Robert L <Robert.Cravens@21co.com>
2022-02-23 17:46:05 -0600

    added mariadb

---
+++ provision/installers/mariadb.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Disable Apparmor
+# See https://github.com/laravel/homestead/issues/629#issue-247524528
+sudo service apparmor stop
+sudo update-rc.d -f apparmor remove
+
+# Remove MySQL
+sudo apt-get remove -y --purge mysql-server mysql-client mysql-common
+sudo apt-get autoremove -y
+sudo apt-get autoclean
+
+sudo rm -rf /var/lib/mysql/*
+sudo rm -rf /var/log/mysql
+sudo rm -rf /etc/mysql
+
+# Add Maria PPA
+sudo curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+
+sudo echo "mariadb-server mysql-server/data-dir select ''" | sudo debconf-set-selections
+sudo echo "mariadb-server mysql-server/root_password password $db_root_password" | sudo debconf-set-selections
+sudo echo "mariadb-server mysql-server/root_password_again password $db_root_password" | sudo debconf-set-selections
+
+sudo mkdir  /etc/mysql
+sudo touch /etc/mysql/debian.cnf
+
+# Install MariaDB
+sudo apt-get install -y mariadb-server mariadb-client
+
+# Configure Maria Remote Access and ignore db dirs
+sudo sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo bash -c 'cat > /etc/mysql/mariadb.conf.d/50-server.cnf << EOF
+[mysqld]
+bind-address = 0.0.0.0
+ignore-db-dir = lost+found
+#general_log
+#general_log_file=/var/log/mysql/mariadb.log
+EOF'
+
+export MYSQL_PWD=$db_root_password
+
+sudo mysql --user="root" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY '$db_root_password' WITH GRANT OPTION;"
+sudo service mysql restart
+
+sudo mysql --user="root" -e "CREATE USER IF NOT EXISTS 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
+sudo mysql --user="root" -e "GRANT ALL ON *.* TO 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+sudo mysql --user="root" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+sudo mysql --user="root" -e "FLUSH PRIVILEGES;"
+sudo service mysql restart
+
+sudo mysql_upgrade --user="root" --verbose --force
+sudo service mysql restart
+
+unset MYSQL_PWD
--- provision/installers/mysql.sh
+++ provision/installers/mysql.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
 # Run the following as root
-[[ $(id -u) -eq 0 ]] || exec sudo /bin/bash -c "$(printf '%q ' "$BASH_SOURCE" "$@")"
+#[[ $(id -u) -eq 0 ]] || exec sudo /bin/bash -c "$(printf '%q ' "$BASH_SOURCE" "$@")"
 
 # Install MySQL
-echo "mysql-server mysql-server/root_password password $db_root_password" |  debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $db_root_password" |  debconf-set-selections
-apt-get install -y mysql-server
+sudo echo "mysql-server mysql-server/root_password password $db_root_password" | sudo debconf-set-selections
+sudo echo "mysql-server mysql-server/root_password_again password $db_root_password" | sudo debconf-set-selections
+sudo apt-get install -y mysql-server
 
 # Configure MySQL 8 Remote Access and Native Pluggable Authentication
- cat > /etc/mysql/conf.d/mysqld.cnf << EOF
+sudo bash -c 'cat > /etc/mysql/conf.d/mysqld.cnf << EOF
 [mysqld]
 bind-address = 0.0.0.0
 default_authentication_plugin = mysql_native_password
-EOF
+EOF'
 
 # Configure MySQL Password Lifetime
- echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Configure MySQL Remote Access
-sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
- service mysql restart
+sudo service mysql restart
 
 export MYSQL_PWD=$db_root_password
 
- mysql --user="root" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_password';"
- mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
- mysql --user="root" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
- mysql --user="root" -e "CREATE USER 'homestead'@'%' IDENTIFIED BY 'secret';"
- mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'0.0.0.0' WITH GRANT OPTION;"
- mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'%' WITH GRANT OPTION;"
- mysql --user="root" -e "FLUSH PRIVILEGES;"
-# mysql --user="root" -e "CREATE DATABASE homestead character set UTF8mb4 collate utf8mb4_bin;"
+sudo mysql --user="root" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_password';"
+sudo mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+sudo mysql --user="root" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
+sudo mysql --user="root" -e "CREATE USER 'homestead'@'%' IDENTIFIED BY 'secret';"
+sudo mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'0.0.0.0' WITH GRANT OPTION;"
+sudo mysql --user="root" -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'%' WITH GRANT OPTION;"
+sudo mysql --user="root" -e "FLUSH PRIVILEGES;"
+# sudo mysql --user="root" -e "CREATE DATABASE homestead character set UTF8mb4 collate utf8mb4_bin;"
 
- tee /home/vagrant/.my.cnf <<EOL
+sudo tee /home/vagrant/.my.cnf <<EOL
 [mysqld]
 character-set-server=utf8mb4
 collation-server=utf8mb4_bin
 EOL
 
- service mysql restart
+sudo service mysql restart
 
 unset MYSQL_PWD
