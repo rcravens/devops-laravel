968c53f27fa31c7125b4f8739288cacffd924229
Cravens, Robert L <Robert.Cravens@21co.com>
2022-02-24 10:07:16 -0600

    fixing

--- deploy/activate.sh
+++ deploy/activate.sh
@@ -11,15 +11,13 @@ if [ "$is_laravel" = true ]; then
 
   if [ ! -d $deploy_directory/cache ]; then
       sudo mkdir $deploy_directory/cache
-      sudo chown -R $username:$username $deploy_directory/cache
   fi
   if [ ! -d public/cache ]; then
-      sudo -u $username /bin/ln -sf $deploy_directory/cache public/cache
+      sudo /bin/ln -sf $deploy_directory/cache public/cache
   fi
 
   if [ ! -d $deploy_directory/data ]; then
       sudo mkdir $deploy_directory/data
-      sudo chown -R $username:$username $deploy_directory/data
   fi
   if [ ! -d public/data ]; then
       sudo /bin/ln -sf $deploy_directory/data public/data
@@ -34,7 +32,6 @@ if [ "$is_laravel" = true ]; then
       sudo mkdir $deploy_directory/storage/framework/sessions
       sudo mkdir $deploy_directory/storage/framework/views
       sudo mkdir $deploy_directory/storage/logs
-      sudo /bin/chown -R $username:$username $deploy_directory/storage
   fi
 
   sudo /bin/rm -rf storage
@@ -44,5 +41,5 @@ fi
 cd $deploy_directory
 sudo /usr/bin/unlink current
 sudo /bin/ln -sf $deploy_directory/releases/$foldername current
-sudo chown $username:$username $deploy_directory/current
 
+sudo chown $username:$username $deploy_directory
