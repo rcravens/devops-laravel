172ad817025285e212cd9ab283b0df5c2766416f
Cravens, Robert L <Robert.Cravens@21co.com>
2022-03-02 17:07:12 -0600

    fix

--- provision/installers/composer.sh
+++ provision/installers/composer.sh
@@ -2,7 +2,7 @@
 
 # Install Composer
 sudo curl -sS https://getcomposer.org/installer | sudo php
-sudo mv composer.phar /usr/local/bin/composer
+sudo mv composer.phar /usr/bin/composer
 
 
 # Install Global Packages
