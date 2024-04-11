12bf8213c7568c18ee058b8cb3657a4c9263b023
rcravens <bob.cravens@gmail.com>
2024-04-11 13:23:01 -0500

    spike on create_app

--- deploy/activate.sh
+++ deploy/activate.sh
@@ -5,6 +5,8 @@
 # create symlinks
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+source ../config.sh
+
 deploy_directory=$PWD
 echo "pwd=$PWD"
 echo "deploy_director=$deploy_directory"
