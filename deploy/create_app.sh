01550fb73a26578cec5cab3236fb22ae34147c9e
rcravens <bob.cravens@gmail.com>
2024-04-11 10:06:14 -0500

    spike on create_app

---
+++ deploy/create_app.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function title {
+    echo "-------------------------------------"
+    echo ""
+    echo "$1"
+    echo ""
+    echo "-------------------------------------"
+}
+
+# Save current directory and cd into script path
+initial_working_directory=$(pwd)
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+# Load the config file
+source ../config.sh
+
+# Create the deployment user
+sudo userdel -r $username
+sudo adduser --gecos "" --disabled-password $username
+sudo chpasswd <<<"$username:$password"
+
+# Start a new session with this new user
+sudo su - $username <<EOF
+
+# Create the Github keys
+ssh-keygen -f ~/.ssh/github_rsa -t rsa -N ""
+cat <<EOT >> ~/.ssh/config
+Host github.com
+        IdentityFile ~/.ssh/github_rsa
+        IdentitiesOnly yes
+EOT
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/*
+
+echo "----------------------COPY PUB KEY TO GITHUB DEPLOYMENT KEYS---------------------"
+cat < ~/.ssh/github_rsa.pub
+echo "---------------------------------------------------------------------------------"
+
+# End session
+exit
+
+EOF
+
+# Return back to the original directory
+cd $initial_working_directory
