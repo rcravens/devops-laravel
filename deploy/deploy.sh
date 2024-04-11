2481efd6cdb0facc445c684d2740aa62d94ee1ea
rcravens <bob.cravens@gmail.com>
2024-04-11 10:23:51 -0500

    spike on deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -17,78 +17,77 @@ cd "$parent_path"
 source ../config.sh
 
 deploy_directory=/home/$username/code
-echo $deploy_directory
-exit
+if [ ! -d $deploy_directory ]; then
+  mkdir -p $deploy_directory
+fi
 
-{
-    # git short hash of remote repo
-    if [ -d $deploy_directory/current ]; then
-      cd $deploy_directory/current/
-      remote_git_line=$(git ls-remote | head -n 1)
-      remote_hash=${remote_git_line:0:7}
-      local_hash=$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/")
-      echo "remote_hash=$remote_hash, local_hash=$local_hash"
-      if [ $remote_hash = $local_hash ]; then
-        echo "Nothing new to deploy."
-        exit 1
-      fi
-    fi
-
-    # create a directory for git clone
-    foldername=$remote_hash
-    title "Deploying: $foldername"
-
-    # create the directory structure
-    if [ ! -d $deploy_directory/releases ]; then
-        sudo mkdir -p $deploy_directory/releases
-        sudo chown -R $username:$username $deploy_directory/releases
-    fi
-    cd $deploy_directory/releases
-    echo  "folder=$deploy_directory/releases/$foldername"
-
-    # git clone into this new directory
-    sudo -u $username git clone --depth 1 $repo $foldername
-    cd $deploy_directory/releases/$foldername
-    sudo chown -R $username:$username $deploy_directory/releases/$foldername
-
-    # composer install
-    title "Dependencies"
-    sudo -u $username /usr/bin/composer install
-    sudo -u $username /usr/bin/npm install
-
-    # create symlinks
-    title "Activation"
-    is_new_dot_env=false
-    if [ ! -f $deploy_directory/.env ]; then
-      is_new_dot_env=true
-    fi
-    source $parent_path/activate.sh
-
-    # migrations
-    if [ "$is_laravel" = true ]; then
-      if [ ! -f $deploy_directory/.env ]; then
-          echo "NO .env FILE FOUND AT $deploy_directory/.env"
-      else
-        title "Migrations"
-        cd $deploy_directory/releases/$foldername
-        if [ "$is_new_dot_env" = true ]; then
-          sudo -u $username php artisan key:generate
-        fi
-        sudo -u $username php artisan migrate --force
-
-        # publish git hash into .env
-        sudo -u $username sed -i "s|GIT_HASH=.*|GIT_HASH=$foldername|" $deploy_directory/.env
-      fi
-    fi
-
-    # restart services
-    title "Restarting"
-    source $parent_path/restart.sh
-
-    # cleanup
-    title "Cleanup"
-    source $parent_path/clean_up.sh
-} 2>&1
+# git short hash of remote repo
+if [ -d $deploy_directory/current ]; then
+  cd $deploy_directory/current/
+  remote_git_line=$(git ls-remote | head -n 1)
+  remote_hash=${remote_git_line:0:7}
+  local_hash=$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/")
+  echo "remote_hash=$remote_hash, local_hash=$local_hash"
+  if [ $remote_hash = $local_hash ]; then
+    echo "Nothing new to deploy."
+    exit 1
+  fi
+fi
+#
+## create a directory for git clone
+#foldername=$remote_hash
+#title "Deploying: $foldername"
+#
+## create the directory structure
+#if [ ! -d $deploy_directory/releases ]; then
+#    sudo mkdir -p $deploy_directory/releases
+#    sudo chown -R $username:$username $deploy_directory/releases
+#fi
+#cd $deploy_directory/releases
+#echo  "folder=$deploy_directory/releases/$foldername"
+#
+## git clone into this new directory
+#sudo -u $username git clone --depth 1 $repo $foldername
+#cd $deploy_directory/releases/$foldername
+#sudo chown -R $username:$username $deploy_directory/releases/$foldername
+#
+## composer install
+#title "Dependencies"
+#sudo -u $username /usr/bin/composer install
+#sudo -u $username /usr/bin/npm install
+#
+## create symlinks
+#title "Activation"
+#is_new_dot_env=false
+#if [ ! -f $deploy_directory/.env ]; then
+#  is_new_dot_env=true
+#fi
+#source $parent_path/activate.sh
+#
+## migrations
+#if [ "$is_laravel" = true ]; then
+#  if [ ! -f $deploy_directory/.env ]; then
+#      echo "NO .env FILE FOUND AT $deploy_directory/.env"
+#  else
+#    title "Migrations"
+#    cd $deploy_directory/releases/$foldername
+#    if [ "$is_new_dot_env" = true ]; then
+#      sudo -u $username php artisan key:generate
+#    fi
+#    sudo -u $username php artisan migrate --force
+#
+#    # publish git hash into .env
+#    sudo -u $username sed -i "s|GIT_HASH=.*|GIT_HASH=$foldername|" $deploy_directory/.env
+#  fi
+#fi
+#
+## restart services
+#title "Restarting"
+#source $parent_path/restart.sh
+#
+## cleanup
+#title "Cleanup"
+#source $parent_path/clean_up.sh
 
 # Return back to the original directory
 cd $initial_working_directory
