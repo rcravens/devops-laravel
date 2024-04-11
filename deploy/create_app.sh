8467620f01ec058fd227b9796adb8ada1a988843
rcravens <bob.cravens@gmail.com>
2024-04-11 16:03:37 -0500

    spike on create_app

--- deploy/create_app.sh
+++ deploy/create_app.sh
@@ -114,6 +114,7 @@ fi
 # Create supervisor conf
 if [ ! -f /etc/supervisor/conf.d/$username.conf ]; then
     sudo cp $parent_path/horizon.conf /etc/supervisor/conf.d/$username.conf
+    sudo sed -i "s|program:|program:$username|" /etc/supervisor/conf.d/$username.conf
     sudo sed -i "s|command=|command=php $deploy_directory/current/artisan horizon|" /etc/supervisor/conf.d/$username.conf
     sudo sed -i "s|user=|user=$username|" /etc/supervisor/conf.d/$username.conf
     sudo sed -i "s|stdout_logfile=|stdout_logfile=$deploy_directory/current/storage/logs/horizon.log|" /etc/supervisor/conf.d/$username.conf
