47afd3baedefd00e7088ba704cf7aae307215d04
rcravens <bob.cravens@gmail.com>
2024-04-12 10:10:33 -0500

    spike on deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -83,7 +83,7 @@ test()
   #title "Cleanup"
   #source $parent_path/clean_up.sh
 }
-sudo su $username -c "test"
+sudo su $username build.sh
 
 # Return back to the original directory
 cd $initial_working_directory
--- deploy/test.sh
+++ deploy/test.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
 
-# Save current directory and cd into script path
-initial_working_directory=$(pwd)
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-# Load the config file
-source ../config.sh
-
-deploy_directory=/home/$username/deployments
-
-# Create supervisor conf
-if [ ! -f /etc/supervisor/conf.d/$username.conf ]; then
-    sudo cp $parent_path/horizon.conf /etc/supervisor/conf.d/$username.conf
-    sudo sed -i "s|program:|program:horizon_$username|" /etc/supervisor/conf.d/$username.conf
-    sudo sed -i "s|command=|command=php $deploy_directory/current/artisan horizon|" /etc/supervisor/conf.d/$username.conf
-    sudo sed -i "s|user=|user=$username|" /etc/supervisor/conf.d/$username.conf
-    sudo sed -i "s|stdout_logfile=|stdout_logfile=$deploy_directory/current/storage/logs/horizon.log|" /etc/supervisor/conf.d/$username.conf
-    sudo supervisorctl reread
-    sudo supervisorctl update
+if [ ! -d $deploy_directory ]; then
+  mkdir -p $deploy_directory
 fi
 
-# Return back to the original directory
-cd $initial_working_directory || exit
+echo "date_string=$date_string"
+
+# git short hash of remote repo
+if [ -d $deploy_directory/current ]; then
+  cd $deploy_directory/current/
+  export remote_git_line=$(git ls-remote | head -n 1)
+  echo "remote_git_line=$remote_git_line"
+  echo $(git ls-remote | head -n 1)
+  remote_hash=${remote_git_line:0:7}
+  local_hash=$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/")
+  echo "remote_hash=$remote_hash, local_hash=$local_hash"
+#  if [ $remote_hash = $local_hash ]; then
+#    echo "Nothing new to deploy."
+#    exit 1
+#  fi
+fi
+
+# create a directory for git clone
+foldername="$date_string-$remote_hash"
+echo "Deploying: $foldername"
+
+# create the directory structure
+if [ ! -d $deploy_directory/releases ]; then
+    mkdir -p $deploy_directory/releases
+fi
+cd $deploy_directory/releases
+echo  "folder=$deploy_directory/releases/$foldername"
+
+# git clone into this new directory
+#git clone --depth 1 $repo $foldername
+#cd $deploy_directory/releases/$foldername
+
+# create symlinks
+#title "Create symlinks"
+#source $parent_path/create_symlinks.sh
+
+# build the application
+#source $parent_path/build.sh
+
+# Activate this version
+#title "Activate"
+#source $parent_path/activate.sh
+
+
+## restart services
+#title "Restarting"
+#source $parent_path/restart.sh
+#
+## cleanup
+#title "Cleanup"
+#source $parent_path/clean_up.sh
