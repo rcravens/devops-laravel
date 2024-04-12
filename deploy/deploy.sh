a79fdea6701a3f0af0c35f294e09b82de93c2b65
rcravens <bob.cravens@gmail.com>
2024-04-12 11:46:42 -0500

    spike on deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-function title {
-    echo "-------------------------------------"
-    echo ""
-    echo "$1"
-    echo ""
-    echo "-------------------------------------"
-}
-function status {
-  BLUE='\033[0;34'
-  NC='\033[0m'
-  echo ""
-  echo -e "${BLUE}--------->$ $1${NC}"
-}
-function error {
-  RED='\033[0;31m'
-  NC='\033[0m'
-  echo ""
-  echo -e "${RED}$1${NC}"
-  echo ""
-}
+source ../helpers.sh
 
 # Save current directory and cd into script path
 initial_working_directory=$(pwd)
