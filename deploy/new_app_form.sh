3d6de88284909e5359a55d790002d026dc27193b
rcravens <bob.cravens@gmail.com>
2024-04-13 18:54:37 -0500

    spike on app form

--- deploy/new_app_form.sh
+++ deploy/new_app_form.sh
@@ -8,14 +8,12 @@ my_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source $my_path/../common/helpers.sh
 
 # Get user name
-echo "Enter the deployment username (e.g., demo_1):"
-read username
+read -p "Enter the deployment username (e.g., demo_1):" username
 
 # Check if the user exists
 while [ -z "$username" ] || [ $(getent passwd "$username") ]; do
   echo "The username '$username' already exists."
-  echo "Enter another deployment username:"
-  read username
+  read -p "Enter another deployment username:" username
 done
 
 # Enter deployment user password
@@ -43,11 +41,11 @@ if [[ "$mysql_password" != "$confirm_mysql_password" ]]; then
 fi
 
 # Enter the application port number
-read -s -p "Application port number (e.g., 8000): " application_port
+read -p "Application port number (e.g., 8000): " application_port
 echo
 
 # Enter the git repo url
-read -s -p "Git repo url: " git_repo_url
+read -p "Git repo url: " git_repo_url
 echo
 
 php_version="8.3"
@@ -57,14 +55,24 @@ read -p "Is this a Laravel app?  (y or Y for Yes): " yes_or_no
 echo
 
 # Check if the input is valid
-is_laravel=0
+is_laravel=false
 if [[ $yes_or_no == "y" ]] || [[ $yes_or_no == "Y" ]]; then
-  is_laravel=1
+  is_laravel=true
 fi
 
 # Public SSH Key for Remote Access As Deployment User
-read -s -p "Public SSH Key for Remote Access As Deployment User: " public_ssh_key
+read -p "Public SSH Key for Remote Access As Deployment User: " public_ssh_key
 echo
+
+cp $my_path/_app.sh $my_path/../apps/$username.sh
+sed -i "s|username=UNAME|username=$username|" $my_path/../apps/$username.sh
+sed -i "s|password=PWORD|password=$password|" $my_path/../apps/$username.sh
+sed -i "s|db_password=DB_PWORD|db_password=$mysql_password|" $my_path/../apps/$username.sh
+sed -i "s|app_port=PORT|app_port=$port|" $my_path/../apps/$username.sh
+sed -i "s|repo=REPO_URL|repo=$repo|" $my_path/../apps/$username.sh
+sed -i "s|php_version=PHP_VERSION|php_version=$php_version|" $my_path/../apps/$username.sh
+sed -i "s|is_laravel=IS_LARAVEL|is_laravel=$is_laravel|" $my_path/../apps/$username.sh
+sed -i "s|public_ssh_key=PUB_SSH_KEY|public_ssh_key=$public_ssh_key|" $my_path/../apps/$username.sh
 
 
 # Return back to the original directory
