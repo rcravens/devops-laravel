51a9ec631736f7d068d69a69cea779e79e5a911e
rcravens <bob.cravens@gmail.com>
2024-04-13 15:16:32 -0500

    created load common

--- common/app_config.sh
+++ common/app_config.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-common_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
 # Expecting an app_name variable
 if [[ ! -v app_name ]]; then
   error "Variable app_name is not set"
@@ -9,10 +7,10 @@ if [[ ! -v app_name ]]; then
 fi
 
 # Load the application configuration file
-app_config_file=$common_path/../apps/$app_name.sh
+app_config_file=$root_path/apps/$app_name.sh
 if [[ ! -f $app_config_file ]]; then
   error "Configuration file not found: $app_config_file"
-  existing_apps=$(ls $common_path/../apps/ | sed -e 's|\.[^.]*$||')
+  existing_apps=$(ls $root_path/apps/ | sed -e 's|\.[^.]*$||')
   error "Try one of these applications:"
   echo "$existing_apps"
   echo ""
