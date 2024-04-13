24ce2323697e4e85039dec6ce57212e405d4bb48
rcravens <bob.cravens@gmail.com>
2024-04-13 17:53:15 -0500

    created delete_app feature

--- common/create_aliases.sh
+++ common/create_aliases.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-alias_str="alias create_app='/usr/local/bin/devops/deploy/create_app.sh'"
-
 if [ ! -f ~/.bash_aliases ]; then
     touch ~/.bash_aliases
 fi
+
+declare -a aliases=(
+"alias create_app='/usr/local/bin/devops/deploy/create_app.sh'"
+"alias create_app='/usr/local/bin/devops/deploy/create_app.sh'"
+                )
+
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
+
+exit
+
+alias_str="alias create_app='/usr/local/bin/devops/deploy/create_app.sh'"
 if grep -q "$alias_str" ~/.bash_aliases; then
   echo "Alias Already Exists: /home/$username/.bash_aliases"
 else
