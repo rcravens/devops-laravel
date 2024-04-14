d5d1c434c605dda96051d8c87dbb711300a07094
rcravens <bob.cravens@gmail.com>
2024-04-14 09:25:27 -0500

    preparing for potentially supporting more than just laravel

--- deploy/builders/laravel/init_symlink_data.sh
+++ deploy/builders/laravel/init_symlink_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "root_directory=$root_directory"
+# Assumption: We are in the deployed directory
+echo "Current Directory: $PWD"
+symlink_directory="$PWD/../../"
+echo "Symlink directory: $symlink_directory"
 
 # Save current directory and cd into script path
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
@@ -11,42 +14,41 @@ source $parent_path/../../../common/helpers.sh
 # Load the config file
 source $parent_path/../../../config.sh
 
-cd $deploy_directory/current/
 
 # Create the initial symlinked repository
-if [ ! -d $deploy_directory/symlinks ]; then
-  mkdir -p $deploy_directory/symlinks
+if [ ! -d $symlink_directory ]; then
+  mkdir -p $symlink_directory
 fi
 if [ "$app_type" = "laravel" ]; then
 
-  if [ ! -f $deploy_directory/symlinks/.env ]; then
-    cp .env $deploy_directory/symlinks/.env
+  if [ ! -f $symlink_directory/.env ]; then
+    cp .env $symlink_directory/.env
   fi
-  if [ ! -d $deploy_directory/symlinks/public ]; then
-   mkdir -p $deploy_directory/symlinks/public
+  if [ ! -d $symlink_directory/public ]; then
+   mkdir -p $symlink_directory/public
   fi
-  if [ ! -d $deploy_directory/symlinks/public/cache ]; then
-   cp -r public/cache $deploy_directory/symlinks/public/cache
+  if [ ! -d $symlink_directory/public/cache ]; then
+   cp -r public/cache $symlink_directory/public/cache
   fi
-  if [ ! -d $deploy_directory/symlinks/public/data ]; then
-   cp -r public/data $deploy_directory/symlinks/public/data
+  if [ ! -d $symlink_directory/public/data ]; then
+   cp -r public/data $symlink_directory/public/data
   fi
-  if [ ! -d $deploy_directory/symlinks/storage ]; then
-    cp -r storage $deploy_directory/symlinks/storage
-    mkdir -p $deploy_directory/symlinks/storage
-    mkdir -p $deploy_directory/symlinks/storage/backups
-    mkdir -p $deploy_directory/symlinks/storage/app
-    mkdir -p $deploy_directory/symlinks/storage/framework
-    mkdir -p $deploy_directory/symlinks/storage/framework/cache
-    mkdir -p $deploy_directory/symlinks/storage/framework/sessions
-    mkdir -p $deploy_directory/symlinks/storage/framework/views
-    mkdir -p $deploy_directory/symlinks/storage/logs
+  if [ ! -d $symlink_directory/storage ]; then
+    cp -r storage $symlink_directory/storage
+    mkdir -p $symlink_directory/storage
+    mkdir -p $symlink_directory/storage/backups
+    mkdir -p $symlink_directory/storage/app
+    mkdir -p $symlink_directory/storage/framework
+    mkdir -p $symlink_directory/storage/framework/cache
+    mkdir -p $symlink_directory/storage/framework/sessions
+    mkdir -p $symlink_directory/storage/framework/views
+    mkdir -p $symlink_directory/storage/logs
   fi
-  if [ ! -f $deploy_directory/symlinks/database.sqlite ]; then
-    cp -r database/database.sqlite $deploy_directory/symlinks/database.sqlite
+  if [ ! -f $symlink_directory/database.sqlite ]; then
+    cp -r database/database.sqlite $symlink_directory/database.sqlite
   fi
 fi
 
 # Activate the newly created symlink sources
 title "Recreating Symlinks"
-source $parent_path/deploy_symlinks.sh
+source $parent_path/init_symlinks.sh
