6339e5b2e27a4e24b3842cb817b9e9939245508d
rcravens <bob.cravens@gmail.com>
2024-04-13 20:40:17 -0500

    preparing for potentially supporting more than just laravel

--- deploy/builders/laravel/symlinks.sh
+++ deploy/builders/laravel/symlinks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Save current directory and cd into script path
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+#parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # Load the helpers
-source $parent_path/../common/helpers.sh
+#source $parent_path/../common/helpers.sh
 
 # Load the config file
-source $parent_path/../config.sh
+#source $parent_path/../config.sh
 
 # Assumption: We are in the deployed directory
 status "Current Directory: $PWD"
