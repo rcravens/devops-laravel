acb8a433c59deb16f1d23bb39a4dab09d27c4b72
rcravens <bob.cravens@gmail.com>
2024-04-13 19:03:38 -0500

    spike on app form

--- deploy/new_app_form.sh
+++ deploy/new_app_form.sh
@@ -7,6 +7,8 @@ my_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # Load common
 source $my_path/../common/helpers.sh
 
+title "Create New Application"
+
 # Get user name
 read -p "Enter the deployment username (e.g., demo_1):" username
 
@@ -42,17 +44,14 @@ fi
 
 # Enter the application port number
 read -p "Application port number (e.g., 8000): " application_port
-echo
 
 # Enter the git repo url
 read -p "Git repo url: " git_repo_url
-echo
 
 php_version="8.3"
 
 # Prompt the user if the app is Laravel application
 read -p "Is this a Laravel app?  (y or Y for Yes): " yes_or_no
-echo
 
 # Check if the input is valid
 is_laravel=false
@@ -62,18 +61,28 @@ fi
 
 # Public SSH Key for Remote Access As Deployment User
 read -p "Public SSH Key for Remote Access As Deployment User: " public_ssh_key
-echo
 
 sudo cp $my_path/_app.sh $my_path/../apps/$username.sh
 sudo sed -i "s|username=UNAME|username=$username|" $my_path/../apps/$username.sh
 sudo sed -i "s|password=PWORD|password=$password|" $my_path/../apps/$username.sh
 sudo sed -i "s|db_password=DB_PWORD|db_password=$mysql_password|" $my_path/../apps/$username.sh
-sudo sed -i "s|app_port=PORT|app_port=$port|" $my_path/../apps/$username.sh
-sudo sed -i "s|repo=REPO_URL|repo=$repo|" $my_path/../apps/$username.sh
+sudo sed -i "s|app_port=PORT|app_port=$application_port|" $my_path/../apps/$username.sh
+sudo sed -i "s|repo=REPO_URL|repo=$git_repo_url|" $my_path/../apps/$username.sh
 sudo sed -i "s|php_version=PHP_VERSION|php_version=$php_version|" $my_path/../apps/$username.sh
 sudo sed -i "s|is_laravel=IS_LARAVEL|is_laravel=$is_laravel|" $my_path/../apps/$username.sh
 sudo sed -i "s|public_ssh_key=PUB_SSH_KEY|public_ssh_key=\"$public_ssh_key\"|" $my_path/../apps/$username.sh
 
+if [ -f $my_path/../apps/$username.sh ]; then
+  echo "A new application config file has been created at:"
+  status "$my_path/../apps/$username.sh"
+  echo ""
+  echo ""
+  echo "Next Steps:"
+  status "1. Review the config file"
+  status "2. Create the application by executing: create_app $username"
+else
+  error "Failed to create a new application config file."
+fi
 
 # Return back to the original directory
 cd $initial_working_directory || exit
