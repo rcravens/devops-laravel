6f48d2ff7e83378581906bfd8e646d51f860bd2a
rcravens <bob.cravens@gmail.com>
2024-04-14 13:34:58 -0500

    ensures the apps directory exists

--- deploy/list_apps.sh
+++ deploy/list_apps.sh
@@ -5,6 +5,11 @@ my_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source $my_path/../common/helpers.sh
 
 title "Existing Applications"
+# Ensure the apps directory exists
+
+if [[ ! -d $my_path/../apps ]]; then
+  mkdir $my_path/../apps
+fi
 
 existing_apps=$(ls $my_path/../apps/ | sed -e 's|\.[^.]*$||')
 echo "$existing_apps"
